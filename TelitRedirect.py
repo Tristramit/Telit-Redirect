ea758a0675b282d5a5e728b34cb748be4774f3a0
Amos Weislib <amosweislib@gmail.com>
2024-01-19 16:54:35 +0100

    First Commit

---
+++ TelitRedirect.py
@@ -0,0 +1,24 @@
+import webbrowser
+from datetime import datetime
+
+def open_url_with_id():
+    while True:
+        # Get user input for the ID
+        user_id = input("Enter the ID (or type 'exit' to end): ")
+
+        # Check if the user wants to exit
+        if user_id.lower() == 'exit':
+            break
+
+        # Get today's date in the desired format (YYYY-MM-DD)
+        today_date = datetime.now().strftime("%Y-%m-%d")
+
+        # Construct the URL using the provided ID and today's date
+        url = f"https://portal-de.telit.com/things/property/{user_id}/prop_temperature?start={today_date}+10%3A00%3A00&end={today_date}+23%3A59%3A59"
+
+        print(f"Opening URL: {url}")
+        # Open the URL in the default web browser
+        webbrowser.open(url)
+
+if __name__ == "__main__":
+    open_url_with_id()
